9046299ef319f42c7c1d3d4c4958600be32ef3b6
Gabriel Melim <gabrielmelim2012@hotmail.com>
2022-04-11 00:19:22 -0200

    Atualizando Projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
-import { LoginComponent } from './login/login.component';
-import { CadastroComponent } from './cadastro/cadastro.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     RodapeComponent,
-    LoginComponent,
-    CadastroComponent,
-    EntrarComponent
+    EntrarComponent,
+    CadastrarComponent,
   ],
   imports: [
     BrowserModule,
